23090cf61be14f505763bf4c83bf3efe9d9428a3
Nurzhan Aitbayev <nurzhan@outlook.com>
2023-04-02 06:45:26 +0600

    Remove unused using

--- TgBotCommandHandler/Extensions/BotClientCommandHandler.cs
+++ TgBotCommandHandler/Extensions/BotClientCommandHandler.cs
@@ -5,72 +5,71 @@ using TgBotCommandHandler.Attributes;
 using TgBotCommandHandler.Types;
 using TgBotCommandHandler.Utils;
 
-namespace TgBotCommandHandler.Extensions
+namespace TgBotCommandHandler.Extensions;
+
+public static class BotClientCommandHandler
 {
-    public static class BotClientCommandHandler
+    private static readonly Dictionary<string, (Type type, MethodInfo methodInfo)> handlersDictionary = new();
+
+    public static void RegisterCommand<T>(this ITelegramBotClient client) where T : CommandHandler
     {
-        private static readonly Dictionary<string, (Type type, MethodInfo methodInfo)> handlersDictionary = new();
+        var methods = typeof(T).GetMethods().Where(method =>
+                method.GetParameters()
+                    .Length == 1
+                && CommandHandlerUtils.IsSameOrSubclass(method.GetParameters()[0].ParameterType,
+                    typeof(CommandContext))
+                && method.IsAsync()
+                && CommandHandlerUtils.IsSameOrSubclass(method.ReturnType, typeof(Task))
+                && method.GetCustomAttributes(typeof(CommandAttribute), false)?.Any() == true)
+            .ToList();
 
-        public static void RegisterCommand<T>(this ITelegramBotClient client) where T : CommandHandler
+        foreach (var method in methods)
         {
-            var methods = typeof(T).GetMethods().Where(method =>
-                    method.GetParameters()
-                        .Length == 1
-                    && CommandHandlerUtils.IsSameOrSubclass(method.GetParameters()[0].ParameterType,
-                        typeof(CommandContext))
-                    && method.IsAsync()
-                    && CommandHandlerUtils.IsSameOrSubclass(method.ReturnType, typeof(Task))
-                    && method.GetCustomAttributes(typeof(CommandAttribute), false)?.Any() == true)
-                .ToList();
+            var commandAttribute = method
+                    .GetCustomAttribute(typeof(CommandAttribute), false)
+                as CommandAttribute;
 
-            foreach (var method in methods)
+            var key = commandAttribute?.Value;
+
+            if (string.IsNullOrWhiteSpace(key))
             {
-                var commandAttribute = method
-                        .GetCustomAttribute(typeof(CommandAttribute), false)
-                    as CommandAttribute;
-
-                var key = commandAttribute?.Value;
-
-                if (string.IsNullOrWhiteSpace(key))
-                {
-                    continue;
-                }
-
-                if (handlersDictionary.ContainsKey(key))
-                {
-                    continue;
-                }
-
-                handlersDictionary.Add(key, (typeof(T), method));
+                continue;
             }
+
+            if (handlersDictionary.ContainsKey(key))
+            {
+                continue;
+            }
+
+            handlersDictionary.Add(key, (typeof(T), method));
         }
+    }
 
         
-        public static async Task HandleCommands(this ITelegramBotClient client, Update e)
-        {
-            var message = e.Message;
+    public static async Task HandleCommands(this ITelegramBotClient client, Update e)
+    {
+        var message = e.Message;
             
-            if (message?.Text?[0] != '/')
-            {
-                return;
-            }
-
-            var command = message.Text.Substring(1).ToLower();
-
-            if (!handlersDictionary.ContainsKey(command))
-            {
-                return;
-            }
-
-            var val = handlersDictionary[command];
-            var commandHandler = (CommandHandler)Activator.CreateInstance(val.type);
-
-
-            var helper = new CommandContext(message, client);
-
-            await (Task)val.methodInfo.Invoke(commandHandler, new object[] { helper });
-
+        if (message?.Text?[0] != '/')
+        {
+            return;
         }
 
+        var command = message.Text.Substring(1).ToLower();
+
+        if (!handlersDictionary.ContainsKey(command))
+        {
+            return;
+        }
+
+        var val = handlersDictionary[command];
+        var commandHandler = (CommandHandler)Activator.CreateInstance(val.type);
+
+
+        var helper = new CommandContext(message, client);
+
+        await (Task)val.methodInfo.Invoke(commandHandler, new object[] { helper });
+
     }
+
 }
--- TgBotCommandHandler/Types/CommandContext.cs
+++ TgBotCommandHandler/Types/CommandContext.cs
@@ -1,7 +1,6 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace TgBotCommandHandler.Types;
 
